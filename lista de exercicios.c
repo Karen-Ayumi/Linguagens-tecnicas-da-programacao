7e242cff0fcd493ca08cb6daf3aeccfa033d37fc
Karen-Ayumi <kgusikuda@gmail.com>
2026-08-13 11:16:07 -0300

    Add files via upload

---
+++ lista de exercicios.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+int primeiro, segundo, aux;
+printf("Insira o primeiro valor");
+scanf("%d", &primeiro);
+printf("Insira o segundo valor");
+scanf("%d", &segundo);
+
+aux = primeiro;
+primeiro = segundo;
+segundo = aux;
+
+printf ("%d \n %d", primeiro, segundo);
+
+double numero, conotacao;
+printf("\nInsira o valor de um numero inteiro positivo");
+scanf("%lf", &numero); 
+
+aux = numero 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
