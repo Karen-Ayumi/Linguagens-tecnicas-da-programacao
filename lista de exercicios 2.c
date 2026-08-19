b544f5c7ea8132742c01f735c0ecfc986790ad8e
Karen-Ayumi <kgusikuda@gmail.com>
2026-08-18 23:02:30 -0300

    Add calculations for various exercises in C

--- lista de exercicios 2.c
+++ lista de exercicios 2.c
@@ -9,24 +9,28 @@ int main(int argc, char *argv[])
  ano = 2026;
  nascimento = ano - idade;
  printf("%d", nascimento); 
+ 
 //2
  float km, ms;
  printf("Coloque o valor em km/s: ");
  scanf("%f", &km);
  ms = km / 36;
  printf("%f", km);
+ 
 //3
  float reais, dolar;
  printf("Coloque o valor em reais: ");
  scanf("%f", &reais);
  dolar = reais / 5.22;
  printf("%f", dolar);
+ 
 //4
 float C, F;
 printf("Coloque o valor em celsius: ");
 scanf("%f", &C);
 F = C * (9.0/5.0) + 32.0;
 printf("%f", F);
+ 
 //5
 float graus, radianos, p;
 printf("Coloque o valor em graus: ");
@@ -34,6 +38,7 @@ scanf("%f", &graus);
 p = 3.141592;
 radianos = graus * (p/ 180);
 printf("%f", radianos);
+ 
 //6
 int numero, sucessor, antecessor;
 printf("Escolha um numero: ");
@@ -41,6 +46,7 @@ scanf("%d", &numero);
 sucessor = numero + 1;
 antecessor = numero - 1;
 printf("%d\n%d", antecessor, sucessor);
+ 
 //7
 float primeiro. segundo, terceiro, premio;
 premio = 780000;
@@ -48,6 +54,7 @@ primeiro = premio * 0.46;
 segundo = premio * 0.32;
 terceiro = premio - (primeiro + segundo);
 printf("%f \n %f \n %f", primeiro, segundo, terceiro);
+ 
 //8
 int horas, minutos, segundos, restominutos;
 printf("Coloque os segundos: ");
@@ -57,6 +64,7 @@ restominutos = segundos % 3600;
 minutos = restominutos /60;
 segundos = restominutos % 60;
 printf("%d:%d:%d", horas, minutos, segundos);
+ 
 //9
 
 
