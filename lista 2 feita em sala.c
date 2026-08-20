a7b1bf018a6a45d292eb715d3727a0b37eadbfca
Karen-Ayumi <kgusikuda@gmail.com>
2026-08-20 11:17:06 -0300

    Add files via upload

---
+++ lista 2 feita em sala.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+
+int main(int argc, char *argv[]) {
+	int a, b, c, d maior, maior_temp, maiort;
+	printf("Insire os valores a serem comparados");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	//conta
+	
+	maior_temp = (a + b + abs(a - b)) / 2;
+	maior = (maior_temp + c + abs(maior_temp - c) ) / 2;
+	
+	printf("O maior entre |%d|%d|%d = %d", a, b, c, maior);
+	
+	*/
+	
+	// 4 valores
+	
+	
+	
+	int main(int argc, char *argv[]) {
+	int a, b, c, d, maior, maior_temp, maiort;
+	printf("Insire os valores a serem comparados");
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	
+	//conta
+	
+	maior_temp = (a + b + abs(a - b)) / 2;
+	maior = (maior_temp + c + abs(maior_temp - c) ) / 2;
+	maiort = ( maior + d + abs(maior - d)) / 2;
+	
+	printf("O maior entre |%d|%d|%d|%d = %d", a, b, c, d, maiort);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
