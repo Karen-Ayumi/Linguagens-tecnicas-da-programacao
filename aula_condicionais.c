575b345cc231a211f15bfefcf49659de1dbc1407
Karen-Ayumi <kgusikuda@gmail.com>
2026-08-26 11:17:40 -0300

    Add files via upload

---
+++ aula_condicionais.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	/*int a, b, c, r;
+	printf("Coloque os valores para A B C: " );
+	scanf("%d%d%d", &a, &b, &c);
+	
+	if(a > b){
+	    r = a;
+	}
+	
+	else {
+	
+        r = b;    
+	}
+	 
+	if (c > r){
+		r = c;
+	}
+	
+	printf("%d, e o maior", r);
+	
+	*/
+	
+	int n;
+	printf("Informe um numero: ");
+	scanf("%d", &n);
+	
+	if (n % 2 == 0){
+		printf("O numero e par");
+	}
+    else {
+    	printf("O numero e impar");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	
+	
+	return 0;
+}
